d224c40c8fada293f26709b9e4b90549d2baa410
Rodrigo Bueno <rodrigo-goncalves.bueno@itau-unibanco.com.br>
2018-09-24 09:26:42 -0300

    Finalizando o Flow de Aceite

--- cordapp/src/main/kotlin/br/com/portabilidade/flow/AceitarFundoPrevidenciaFlow.kt
+++ cordapp/src/main/kotlin/br/com/portabilidade/flow/AceitarFundoPrevidenciaFlow.kt
@@ -4,7 +4,9 @@ import br.com.portabilidade.contract.SolicitacaoContract
 import br.com.portabilidade.model.StatusSolicitacao
 import br.com.portabilidade.state.FundoPrevidenciaState
 import br.com.portabilidade.state.SolicitacaoState
+import co.paralleluniverse.fibers.Suspendable
 import net.corda.core.contracts.Command
+import net.corda.core.contracts.requireThat
 import net.corda.core.flows.*
 import net.corda.core.node.services.queryBy
 import net.corda.core.node.services.vault.QueryCriteria
@@ -70,8 +72,14 @@ object AceitarFundoPrevidenciaFlow {
     @InitiatedBy(Initiator::class)
     class Acceptor(val otherParty: FlowSession): FlowLogic<SignedTransaction>() {
 
+        @Suspendable
         override fun call(): SignedTransaction {
-            
+            val signTransactionFlow = object : SignTransactionFlow(otherParty) {
+                override fun checkTransaction(stx: SignedTransaction) = requireThat {
+                }
+            }
+
+            return subFlow(signTransactionFlow)
         }
     }
 }
