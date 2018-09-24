a2ddc53d0870adce3c718e68d395ec40c643587e
Rodrigo Bueno <rodrigo-goncalves.bueno@itau-unibanco.com.br>
2018-09-24 10:32:12 -0300

    Finalizacao dos contratos

--- cordapp-contracts-states/src/main/kotlin/br/com/portabilidade/contract/FundoPrevidenciaContract.kt
+++ cordapp-contracts-states/src/main/kotlin/br/com/portabilidade/contract/FundoPrevidenciaContract.kt
@@ -1,2 +1,45 @@
 package br.com.portabilidade.contract
 
+import br.com.portabilidade.state.FundoPrevidenciaState
+import net.corda.core.contracts.CommandData
+import net.corda.core.contracts.Contract
+import net.corda.core.contracts.requireThat
+import net.corda.core.transactions.LedgerTransaction
+
+class FundoPrevidenciaContract: Contract {
+
+    override fun verify(tx: LedgerTransaction) {
+        val comando = tx.commandsOfType<Commands>().single()
+
+        when (comando.value) {
+            is Commands.Create -> verifyCreate(tx)
+            else -> throw IllegalArgumentException("Comando ${comando.value::class.java.canonicalName} não reconhecido.")
+        }
+
+        requireThat {
+            "Todos os participantes devem assinar a transação." using
+                    (tx.inputsOfType<FundoPrevidenciaState>() + tx.outputsOfType())
+                            .flatMap {
+                                it.participants.map {
+                                    it.owningKey }
+                            }.containsAll(comando.signers)
+        }
+
+    }
+
+    fun verifyCreate(tx: LedgerTransaction) {
+        val fundosPrevidencia = tx.outputsOfType<FundoPrevidenciaState>().map { it.fundoPrevidencia }
+        requireThat {
+            "Não deve haver inputs." using ( tx.inputsOfType<FundoPrevidenciaState>().isEmpty() )
+            "Deve haver apenas um output." using ( fundosPrevidencia.size == 1 )
+            "O valor do Fundo de Previdência deve ser positivo." using ( fundosPrevidencia.all { it.valor > 0 } )
+            "O Fundo de Previdência precisa ter um contrato." using ( fundosPrevidencia.all { it.idContrato.isNotBlank() } )
+            "O Fundo de Previdência precisa ter um beneficiario." using ( fundosPrevidencia.all { it.beneficiario.isNotBlank() } )
+        }
+    }
+
+    interface Commands: CommandData {
+        class Create: Commands
+    }
+
+}
--- cordapp-contracts-states/src/main/kotlin/br/com/portabilidade/contract/SolicitacaoContract.kt
+++ cordapp-contracts-states/src/main/kotlin/br/com/portabilidade/contract/SolicitacaoContract.kt
@@ -9,15 +9,24 @@ import net.corda.core.contracts.LinearState
 import net.corda.core.contracts.requireThat
 import net.corda.core.transactions.LedgerTransaction
 
-class SolicitacaoContract: Contract {
+class SolicitacaoContract : Contract {
 
     override fun verify(tx: LedgerTransaction) {
         val comando = tx.commandsOfType<Commands>().single()
-        when(comando.value) {
+        when (comando.value) {
             is Commands.Solicitar -> verifySolicitar(tx)
             is Commands.Aceitar -> verifyAceitar(tx)
             is Commands.Recusar -> verifyRecusar(tx)
             else -> throw IllegalArgumentException("Comando desconhecido.")
+        }
+        requireThat {
+            "Todos os participantes devem assinar a transação." using
+                    (tx.inputsOfType<FundoPrevidenciaState>() + tx.outputsOfType())
+                            .flatMap {
+                                it.participants.map {
+                                    it.owningKey
+                                }
+                            }.containsAll(comando.signers)
         }
     }
 
@@ -26,23 +35,23 @@ class SolicitacaoContract: Contract {
             "Não deve haver inputs." using (
                     tx.inputsOfType<SolicitacaoState>().isEmpty())
             "Deve haver apenas um output." using (
-                    tx.outputsOfType<SolicitacaoState>().size == 1 )
+                    tx.outputsOfType<SolicitacaoState>().size == 1)
             "O Status da solicitação deve ser SOLICITADO." using (
                     tx.outputsOfType<SolicitacaoState>().all {
                         it.statusSolicitacao == StatusSolicitacao.SOLICITADO
-                    } )
+                    })
             "O solicitante e o solicitado devem ser diferentes." using (
                     tx.outputsOfType<SolicitacaoState>().all {
                         it.solicitante != it.solicitado
-                    } )
+                    })
             "O numero de identificação não pode ser vazio." using (
                     tx.outputsOfType<SolicitacaoState>().all {
                         it.documentoIdentificacao.isNotBlank()
-                    } )
+                    })
         }
     }
 
-    inline fun <reified T: LinearState> verificarApenasUmInputEOutput(tx: LedgerTransaction){
+    inline fun <reified T : LinearState> verificarApenasUmInputEOutput(tx: LedgerTransaction) {
         requireThat {
             "Deve haver um input de ${T::class.java.canonicalName}." using (
                     tx.inputsOfType<T>().size == 1)
@@ -51,7 +60,7 @@ class SolicitacaoContract: Contract {
         }
     }
 
-    fun verifyAceitar(tx: LedgerTransaction){
+    fun verifyUpdate(tx: LedgerTransaction, novoStatus: StatusSolicitacao) {
         requireThat {
             verificarApenasUmInputEOutput<SolicitacaoState>(tx)
             verificarApenasUmInputEOutput<FundoPrevidenciaState>(tx)
@@ -59,42 +68,52 @@ class SolicitacaoContract: Contract {
             "O Status da Solicitacao deveria ser SOLICITADO." using (
                     tx.inputsOfType<SolicitacaoState>().all {
                         it.statusSolicitacao == StatusSolicitacao.SOLICITADO
-                    } )
-            "O Status da Solicitacao deve ser ACEITO." using (
+                    })
+
+            "O Status da Solicitacao deve ser $novoStatus." using (
                     tx.outputsOfType<SolicitacaoState>().all {
-                        it.statusSolicitacao == StatusSolicitacao.ACEITO
-                    } )
+                        it.statusSolicitacao == novoStatus
+                    })
+
+            val inputSolicitacao = tx.inputsOfType<SolicitacaoState>().single()
+            val outputSolicitacao = tx.outputsOfType<SolicitacaoState>().single()
+
+            "Apenas o Status da Solicitacao deve ser alterado." using (
+                    inputSolicitacao.solicitado == outputSolicitacao.solicitado &&
+                            inputSolicitacao.solicitante == outputSolicitacao.solicitante &&
+                            inputSolicitacao.documentoIdentificacao == outputSolicitacao.documentoIdentificacao
+                    )
+        }
+    }
+
+    fun verifyAceitar(tx: LedgerTransaction) {
+        requireThat {
+            verifyUpdate(tx, StatusSolicitacao.ACEITO)
 
             val outputSolicitacao = tx.outputsOfType<SolicitacaoState>().single()
             val outputFundo = tx.outputsOfType<FundoPrevidenciaState>().single()
 
             "O id do Fundo deve ser igual ao numero de identificação." using (
                     outputSolicitacao.documentoIdentificacao ==
-                            outputFundo.fundoPrevidencia.idContrato )
+                            outputFundo.fundoPrevidencia.idContrato)
 
             val inputFundo = tx.inputsOfType<FundoPrevidenciaState>().single()
-            val inputSolicitacao = tx.inputsOfType<SolicitacaoState>().single()
             "O dono do Fundo deve ser alterado." using (
-                    inputFundo.dono != outputFundo.dono )
-            "Apenas o Status da Solicitacao deve ser alterado." using (
-                    inputSolicitacao.solicitado == outputSolicitacao.solicitado &&
-                            inputSolicitacao.solicitante == outputSolicitacao.solicitante &&
-                            inputSolicitacao.documentoIdentificacao == outputSolicitacao.documentoIdentificacao
-                    )
+                    inputFundo.dono != outputFundo.dono)
             "Apenas o Dono do Fundo deve ser alterado." using (
                     inputFundo.fundoPrevidencia == outputFundo.fundoPrevidencia
                     )
         }
     }
 
-    fun verifyRecusar(tx: LedgerTransaction){
-        TODO()
+    fun verifyRecusar(tx: LedgerTransaction) {
+        verifyUpdate(tx, StatusSolicitacao.RECUSADO)
     }
 
-    interface Commands: CommandData {
-        class Solicitar: Commands
-        class Aceitar: Commands
-        class Recusar: Commands
+    interface Commands : CommandData {
+        class Solicitar : Commands
+        class Aceitar : Commands
+        class Recusar : Commands
     }
 
 }
